23509e2af4df917ff16f829e6fa7a1796f7d971e
Lourdes <3009732330101@ingenieria.usac.edu.gt>
2025-04-11 18:12:47 -0600

    WIP

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 console.log("Hello World")
 console.log("Hello World")
 console.log("Hello World")
+console.log("Hello World2")
 console.log("Hello World")
 console.log("Hello World")
 console.log("Hello World")
